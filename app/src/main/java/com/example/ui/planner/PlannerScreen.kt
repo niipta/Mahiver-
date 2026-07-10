2dac8d8aa5fef0b042b71a23d8e315ccc12f592b
Z User <z@container>
2026-07-10 08:56:16 +0000

    Fix missing SubtopicEntity import in PlannerScreen

--- app/src/main/java/com/example/ui/planner/PlannerScreen.kt
+++ app/src/main/java/com/example/ui/planner/PlannerScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.data.SubtopicEntity
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
