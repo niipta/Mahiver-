0bc0c59f704f725d4777c3f406fe5b3ea4339b67
Z User <z@container>
2026-07-18 06:55:42 +0000

    Bump version 4.3 -> 4.4 (versionCode 12 -> 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.mahirverse.xjklqa"
     minSdk = 24
     targetSdk = 35
-    versionCode = 12
-    versionName = "4.3"
+    versionCode = 13
+    versionName = "4.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
